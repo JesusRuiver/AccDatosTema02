0cf71a74ff01aa5e8b1bea28af2aa29e01f70e29
Jesús <jesusruiver@jesusruiver.com>
2017-11-03 14:27:23 +0100

    Commit

---
+++ src/oracle/MainOracle.java
@@ -0,0 +1,36 @@
+package oracle;
+
+import java.sql.*;
+
+public class MainOracle {
+	public static void main(String[] args) {
+		try {
+			// Cargar el driver
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// Establecemos la conexion con la BD
+			Connection conexion = DriverManager.getConnection("jdbc:oracle:thin:@localhost/EJEMPLO", "ejemplo", "root");
+
+			// Preparamos la consulta
+			Statement sentencia = conexion.createStatement();
+			String sql = "SELECT * FROM departamentos";
+			ResultSet resul = sentencia.executeQuery(sql);
+
+			// Recorremos el resultado para visualizar cada fila
+			// Se hace un bucle mientras haya registros y se van visualizando
+			while (resul.next()) {
+				System.out.printf("%d, %s, %s %n", resul.getInt(1), resul.getString(2), resul.getString(3));
+			}
+
+			resul.close(); // Cerrar ResultSet
+			sentencia.close(); // Cerrar Statement
+			conexion.close(); // Cerrar conexión
+
+		} catch (ClassNotFoundException cn) {
+			cn.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}// fin de main
+}// fin de la clase
