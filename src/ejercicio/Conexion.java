4c9f64a9ec54bd42131c792c766219fa153646eb
JesusRuiver <jesusruiver@jesusruiver.com>
2017-11-13 18:53:55 +0100

    Clase Coneción

---
+++ src/ejercicio/Conexion.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package ejercicio;
+
+import java.sql.Connection;
+
+/**
+ * @author Vespertino
+ *
+ */
+public class Conexion {
+	
+private Connection conexion;
+private String usuario;
+private String clave;
+private String baseDeDatos;
+
+public void conectar (){
+	
+}
+
+public void desconectar(){
+	
+}
+
+public int ejecutarInsert(){
+	int i = 0;
+	
+	return i;
+}
+
+public void ejecutarSelect(){
+	
+}
+
+}
